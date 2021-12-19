dd49a9c86993610f4a00b0aeda2567b8011590b6
Hyun <hyun11732@gmail.com>
2021-12-19 22:16:01 +0900

    test

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import FirebaseContext from './context/firebase';
+import {firebase, FieldValue} from './lib/firebase';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+ReactDOM.render(
+    <FirebaseContext.Provider value={{firebase, FieldValue}}>
+           <App /> 
+    </FirebaseContext.Provider>,
+    document.getElementById('root')
+);
 
+// client side rendered app : react(cra)
+// -> database which is Firevbase
+// react loading skeleton
+// tailwind
+
+//architecture, folderr structure
+    // -> components, 
+    // constrants, 
+    //context, 
+    //helpers, 
+    //lib(firebase is going to live here) 
+    //, services(firebase functions in here) 
+    // -> styles(talwind's folder(app/tailwind)) 
